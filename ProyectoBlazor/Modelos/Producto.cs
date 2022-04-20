9956aa58a5a6c5bda237a41d91c2c80aecbbd916
JRLagos <jrlagosc@unah.hn>
2022-04-19 22:48:41 -0600

    Jorge Parte1
    Primer y Segundo Video Clase

---
+++ ProyectoBlazor/Modelos/Producto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Modelos;
+
+public class Producto
+{
+    [Required(ErrorMessage = "El Campo Codigo Es Obligatorio")]
+    public string Codigo { get; set; }
+    [Required(ErrorMessage = "El Campo Descripcion Es Obligatorio")]
+    public string Descripcion { get; set; }
+    public decimal Precio { get; set; }
+    public int  Existencia { get; set; }
+    public byte[] Imagen { get; set; }
+}
